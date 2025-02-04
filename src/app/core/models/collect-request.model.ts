75275adae1ce5190f2c04df40253a74606eb7812
salah <akilsalaheddine203@gmail.com>
2025-02-04 09:49:56 +0100

    add services

--- src/app/core/models/collect-request.model.ts
+++ src/app/core/models/collect-request.model.ts
@@ -6,5 +6,5 @@ export interface CollectRequest {
     collectionAddress: string; 
     collectionDate: Date; 
     timeSlot: string; 
-    status: 'en attente' | 'occupée' | 'en cours' | 'validée' | 'rejetée'; // Statut de la demande
+    status: 'en attente' | 'occupée' | 'en cours' | 'validée' | 'rejetée'; 
   }
