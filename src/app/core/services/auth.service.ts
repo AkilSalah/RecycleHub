04cfcbbca1ea26e3ffbaa05954b86a6a1d478faa
salah <akilsalaheddine203@gmail.com>
2025-02-04 13:48:46 +0100

    register done

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private readonly STORAGE_KEY = 'users';
+
+  register(user: User): void {
+    const users = this.getUsers();
+    user.id = this.generateId(users);
+    this.saveUsers([...users, user]);
+  }
+
+  getUsers(): User[] {
+    return JSON.parse(localStorage.getItem(this.STORAGE_KEY) || '[]');
+  }
+
+  private saveUsers(users: User[]): void {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(users));
+  }
+
+  private generateId(users: User[]): number {
+    return users.length ? Math.max(...users.map(u => u.id)) + 1 : 1;
+  }
 }
--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../../core/models/user.model';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      address: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
+      birthDate: ['', Validators.required],
+    });
+  }
+
+  // Méthode pour soumettre le formulaire
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const newUser: User = {
+        id: 0, 
+        ...this.registerForm.value,
+        role: 'particulier', 
+      };
+
+      this.authService.register(newUser); 
+      this.router.navigate(['/login']); 
+    }
+  }
 }
